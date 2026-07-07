1032438f0e0a8b135a336988eae880cd28214900
jimmysin069 <jimmysin069@gmail.com>
2026-07-08 01:38:37 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "mmk-thb-v5";
+const CACHE = "mmk-thb-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
